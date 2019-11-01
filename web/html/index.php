01c12dd315ab106c2ebf23fd3bc56d7e4e82ebd9
rhapidfyre <maharris84@hotmail.com>
2019-11-01 14:49:53 -0700

    Added a toggle-able map legend

--- web/html/index.php
+++ web/html/index.php
@@ -1,20 +1,36 @@
 <html>
-  <?php require_once('inc/database.php'); ?>
+  <?php
+  
+    require_once('inc/database.php');
+    
+  ?>
   <head>
     <title>SceneAlert - Real Time Incident Reporting</title>
     <link rel="stylesheet" href="inc/index.css">
     <link rel="stylesheet" href="inc/info.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-    
-    <!-- The Google Map Script -->
+    <script type="text/javascript">
+      var showLegend = true;
+      function ToggleLegend() {
+        showLegend = !showLegend
+        if (showLegend) {
+          $("#legend").show();
+          $("#legshow").hide();
+        } else {
+          $("#legend").hide();
+          $("#legshow").show();
+        }
+      }
+    </script>
+    <!-- The Google Map Script
     <script type="text/javascript" src="js/googlemap.js"></script>
     <script
       async defer
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH1jW5Aqd8lHL7RoiiKx9COdioIRdGs8Q&callback=initMap"
       type="text/javascript">
     </script>
-    
+    -->
   </head>
   
   
@@ -22,6 +38,8 @@
     
     <!-- The Map -->
     <div id="gmap"></div>
+    
+    <?php include('legend.php'); ?>
     
     <!-- Top Bar: Home/About/Account/FAQ/etc -->
     <div id="nav-topbar">
---
+++ web/html/legend.php
@@ -0,0 +1,52 @@
+<?php
+  ?>
+    <div id="legend">
+      <table>
+          <tr>
+            <th><img src="img/pins/1s.png" width="10px" height="16px"/></th>
+            <td>Law Enforcement</td>
+          </tr>
+          <tr>
+            <th><img src="img/pins/2s.png" width="10px" height="16px"/></th>
+            <td>Fire / Rescue</td>
+          </tr>
+          <tr>
+            <th><img src="img/pins/3s.png" width="10px" height="16px"/></th>
+            <td>Medical</td>
+          </tr>
+          <tr>
+            <th><img src="img/pins/4s.png" width="10px" height="16px"/></th>
+            <td>Multi-Service</td>
+          </tr>
+          <tr>
+            <th><img src="img/pins/5s.png" width="10px" height="16px"/></th>
+            <td>Military</td>
+          </tr>
+          <tr>
+            <th><img src="img/pins/6s.png" width="10px" height="16px"/></th>
+            <td>System Admin</td>
+          </tr>
+          <tr>
+          <tr>
+            <th><img src="img/info/alert.png" width="10px" height="16px"/></th>
+            <td>Report Issue</td>
+          </tr>
+          <tr>
+            <th><img src="img/info/msg.png" width="10px" height="16px"/></th>
+            <td>Send Message</td>
+          </tr>
+          <tr>
+            <th><img src="img/info/view.png" width="10px" height="16px"/></th>
+            <td>View Details</td>
+          </tr>
+          <tr>
+          <tr>
+            <td colspan="2"><button onclick="ToggleLegend()">HIDE</button></td>
+          </tr>
+      </table>
+    </div>
+    <div id="legshow" style="display: none;">
+      <button onclick="ToggleLegend()">LEGEND</button>
+    </div>
+  <?php
+?>
