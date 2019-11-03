952ededdca815c6b776b9944a8940dfe04df36cc
rhapidfyre <maharris84@hotmail.com>
2019-11-02 17:25:07 -0700

    Made Google Map dark themed

--- web/html/legend.php
+++ web/html/legend.php
@@ -3,41 +3,41 @@
     <div id="legend">
       <table>
           <tr>
-            <th><img src="img/pins/1s.png" width="10px" height="16px"/></th>
             <td>Law Enforcement</td>
+            <th><img src="img/pins/1s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/pins/2s.png" width="10px" height="16px"/></th>
             <td>Fire / Rescue</td>
+            <th><img src="img/pins/2s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/pins/3s.png" width="10px" height="16px"/></th>
             <td>Medical</td>
+            <th><img src="img/pins/3s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/pins/4s.png" width="10px" height="16px"/></th>
             <td>Multi-Service</td>
+            <th><img src="img/pins/4s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/pins/5s.png" width="10px" height="16px"/></th>
             <td>Military</td>
+            <th><img src="img/pins/5s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/pins/6s.png" width="10px" height="16px"/></th>
             <td>System Admin</td>
+            <th><img src="img/pins/6s.png" width="16px" height="24px"/></th>
           </tr>
           <tr>
           <tr>
-            <th><img src="img/info/alert.png" width="10px" height="16px"/></th>
             <td>Report Issue</td>
+            <th><img src="img/info/alert.png" width="24px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/info/msg.png" width="10px" height="16px"/></th>
             <td>Send Message</td>
+            <th><img src="img/info/msg.png" width="24px" height="24px"/></th>
           </tr>
           <tr>
-            <th><img src="img/info/view.png" width="10px" height="16px"/></th>
             <td>View Details</td>
+            <th><img src="img/info/view.png" width="24px" height="24px"/></th>
           </tr>
           <tr>
           <tr>
