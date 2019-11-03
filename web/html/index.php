bf141167c0e804d6565ce86c36a6d291ae130aa5
rhapidfyre <maharris84@hotmail.com>
2019-11-02 17:25:22 -0700

    Prepping for call creation and login system

--- web/html/index.php
+++ web/html/index.php
@@ -16,12 +16,12 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     
-    <!-- The Google Map Script  -->
+    <!-- The Google Map Script 
     <script type="text/javascript" src="js/googlemap.js"></script>
     <script async defer
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH1jW5Aqd8lHL7RoiiKx9COdioIRdGs8Q&callback=initMap"
       type="text/javascript">
-    </script>
+    </script> -->
     
     <!-- Loose Javascript -->
     <script type="text/javascript">
@@ -36,6 +36,10 @@
           $("#legend").hide();
           $("#legshow").show();
         }
+      }
+      
+      function HideLogin() {
+        $("#acct-details").fadeOut(200);
       }
 
     </script>
@@ -66,11 +70,78 @@
       <input type="submit" value="Submit"/>
       
     </div>
-
-    <?php include('legend.php'); ?>
     
     <!-- Top Bar: Home/About/Account/FAQ/etc -->
     <?php include('topbar.php'); ?>
+    <?php include('legend.php'); ?>
+    
+    
+    <!-- Controls -->
+    <div id="call-control">
+    
+      <h3>Control Panel</h3>
+      
+      <!-- Controls to Search / View -->
+      <div id="call-search">
+        <table>
+          <tr>
+            <th colspan="2">Search for Address</th>
+          </tr>
+          <tr>
+            <td width="70%">
+              <input type="text" id="sr-addr" name="sr-addr" placeholder="123 Easy Street">
+            </td>
+            <td width="30%">
+              <button onclick="SearchAddress()">Search</button>
+            </td>
+          </th>
+        </table>
+      </div>
+      
+      <!-- Controls to create a call or edit existing -->
+      <div id="call-create">
+        <h3>New Incident</h3>
+        <p><button onclick="NewCallHelp()">Help</button>
+        <table>
+          <tr>
+            <td>Address</td>
+            <td><input type="text" id="new-stnumber" name="new-stnumber" placeholder="123"/></td>
+            <td><input type="text" id="new-stname" name="new-stname" placeholder="Easy Street"/></td>
+          </tr>
+          <tr>
+            <td><input type="number" id="new-zip" name="new-zip" placeholder="90210 (opt)"></td>
+            <td><input type="text" id="new-city" name="new-city" placeholder="City Name (opt)"></td>
+            <td><input type="text" id="new-state" name="new-state" placeholder="CA" maxlength="2"></td>
+          </tr>
+          <tr>
+            <td>Situation</td>
+            <td colspan="2">
+              <select>
+                <option value="1">Police Activity</option>
+                <option value="2">Fire / Rescue</option>
+                <option value="3">Medical Emergency</option>
+                <option value="4">Multi-Agency</option>
+                <option value="5">Military Operation</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3">
+              <input type="text" id="new-details" name="new-details" placeholder="Brief Title of Event"/>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3">
+              <textarea></textarea>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="3"><button onclick="NewCallCreate()">Submit</button>
+          </tr>
+        </table>
+      </div>
+      
+    </div>
     
     <!-- Infowindow on Pin Click -->
     <div id="info-window" style="display: none;">
